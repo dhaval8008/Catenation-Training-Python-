b758075882989c0fcc212beb7a71a35dddb4874e
dhaval8008 <46796220+dhaval8008@users.noreply.github.com>
2020-10-21 14:55:54 -0400

    Create Task 3.py

---
+++ Task 3.py
@@ -0,0 +1,30 @@
+#1. Create a list of the 10 elements of four different types of Data Type like int,
+#string, complex and float.
+
+a=[1, 5, 3.5, 6.5, "dhaval", "adhvaryu", 3 + 5j, 44]
+
+
+
+#2. Create a list of size 5 and execute the slicing structure
+
+a=[1 ,2 ,3, 4, 5]
+print (a[:])
+print (a[:2])
+
+#3. Write a program to get the sum and multiply of all the items in a given list.
+
+
+
+#4. Find the largest and smallest number from a given list.
+
+print (max(a))
+print(min(a))
+
+#5. Create a new list which contains the specified numbers after removing the
+#even numbers from a predefined list.
+
+
+
+
+#6. Create a list of first and last 5 elements where the values are square of
+#numbers between 1 and30 (both included).
