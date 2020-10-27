38090bd07c98938d32890a92b65bc45bb08d1ff2
dhaval8008 <46796220+dhaval8008@users.noreply.github.com>
2020-10-26 21:47:57 -0400

    Create Task 6.py

---
+++ Task 6.py
@@ -0,0 +1,48 @@
+'''1. Write a program to Python find the values which is not divisible 3 but is should be
+a multiple of 7. Make sure to use only higher order function.'''
+
+
+
+
+
+'''2. Write a program in Python to multiple the element of list by itself using a
+traditional function and pass the function to map to complete the operation.'''
+
+
+
+
+'''3. Write a program to Python find out the character in a string which is uppercase
+using list comprehension.'''
+
+
+
+'''4. Write a program to construct a dictionary from the two lists containing the names
+of students and their corresponding subjects. The dictionary should maps the
+students with their respective subjects. Let’s see how to do this using for loops and
+dictionary comprehension. HINT-Use Zip function also
+● Student = [&#39;Smit&#39;, &#39;Jaya&#39;, &#39;Rayyan&#39;]
+● capital = [&#39;CSE&#39;, &#39;Networking&#39;, &#39;Operating System&#39;]'''
+
+
+
+
+'''5. Learn More about Yield, next and Generators'''
+
+
+
+
+
+'''6. Write a program in Python using generators to reverse the string. Input String =
+“Consultadd Training”'''
+
+
+
+
+'''7. Write any example on decorators.'''
+
+
+
+'''8. Learn about What is FRONT END and its Technologies and Tools
+● Make sure to mention at least 5 top notch technologies of Frontend.
+● Also mentioned the name of companies using those 5 technologies
+individually'''
